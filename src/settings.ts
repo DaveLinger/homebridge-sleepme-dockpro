92c57aef64b21cc04a947a30fb11b75345f0f232
Dave <22969871+DaveLinger@users.noreply.github.com>
2025-03-16 19:28:39 -0400

    idk

--- src/settings.ts
+++ src/settings.ts
@@ -2,7 +2,6 @@
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
 export const PLATFORM_NAME = 'SleepmeDockProHomebridgePlugin';
-
 /**
  * This must match the name of your plugin as defined the package.json
  */
